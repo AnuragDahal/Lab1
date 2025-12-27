25ff69619e26044a8c670fa8c9fb0269fe678ca6
AnuragDahal <freefirepros2020@gmail.com>
2025-12-27 22:39:46 +0545

    feat: update script to use the joblib model  of the same dir

--- assignment/deploy.py
+++ assignment/deploy.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import joblib
+import os
 
 st.title("Height Weight KNN")
 
-model = joblib.load("model.joblib")
+# Get the directory where the script is located
+base_path = os.path.dirname(__file__)
+model_path = os.path.join(base_path, "model.joblib")
+
+model = joblib.load(model_path)
 
 height = st.number_input("Enter height in cm")
 weight = st.number_input("Enter weight in kg")
